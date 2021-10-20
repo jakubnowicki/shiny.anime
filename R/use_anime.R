d7e24e9c1ff3e431a05f4437a201038ce6dc63a2
Jakub Nowicki <kuba@appsilon.com>
2021-10-20 22:52:42 +0200

    use_anime example.

--- R/use_anime.R
+++ R/use_anime.R
@@ -5,6 +5,41 @@
 #' @export
 #' 
 #' @importFrom htmltools htmlDependency
+#' 
+#' @examples 
+#' \dontrun{
+#' library(shiny)
+#' library(shiny.anime)
+#' 
+#' ui <- fluidPage(
+#'     use_anime(),
+#'     actionButton(
+#'         "move",
+#'         "Move"
+#'     )
+#' )
+#' 
+#' server <- function(input, output, session) {
+#'     position <- reactiveVal(0)
+#' 
+#'     observeEvent(input$move, {
+#'         movement <- position() + 30
+#' 
+#'         if (movement >= 200) {
+#'             movement <- 0
+#'         }
+#' 
+#'         position(movement)
+#' 
+#'         trigger_animation(
+#'             targets = "#move",
+#'             translateX = paste0(movement, "px")
+#'         )
+#'     })
+#' }
+#' 
+#' shinyApp(ui = ui, server = server)
+#' }
 use_anime <- function() {
   htmlDependency(
     name = "anime.js",
